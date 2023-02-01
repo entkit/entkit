70d244024274ee87b6fdb258563535be09300ec8
Aaron Yordanyan <aaron.yor@gmail.com>
2023-02-02 03:02:30 +0400

    IDTree view

---
+++ examples/refine-project/src/ent-refine/main-show.tsx
@@ -0,0 +1,23 @@
+import * as Show from "./show";
+
+export const CompanyMainShow : React.FC = () => {
+    return <Show.CompanyShow withEdges={true}/>
+};export const CountryMainShow : React.FC = () => {
+    return <Show.CountryShow withEdges={true}/>
+};export const EmailMainShow : React.FC = () => {
+    return <Show.EmailShow withEdges={true}/>
+};export const ImageMainShow : React.FC = () => {
+    return <Show.ImageShow withEdges={true}/>
+};export const LocationMainShow : React.FC = () => {
+    return <Show.LocationShow withEdges={true}/>
+};export const PhoneMainShow : React.FC = () => {
+    return <Show.PhoneShow withEdges={true}/>
+};export const ProductMainShow : React.FC = () => {
+    return <Show.ProductShow withEdges={true}/>
+};export const VendorMainShow : React.FC = () => {
+    return <Show.VendorShow withEdges={true}/>
+};export const WarehouseMainShow : React.FC = () => {
+    return <Show.WarehouseShow withEdges={true}/>
+};export const WebsiteMainShow : React.FC = () => {
+    return <Show.WebsiteShow withEdges={true}/>
+};
