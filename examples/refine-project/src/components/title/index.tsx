265833ff3fd92c3b187f2dfabe9b0b2959198122
Aaron Yordanyan <aaron.yor@gmail.com>
2023-02-11 23:16:35 +0400

    Update logo and cover

--- examples/refine-project/src/components/title/index.tsx
+++ examples/refine-project/src/components/title/index.tsx
@@ -1,18 +1,22 @@
 import {Logo} from "./styled";
 import {Icons} from "@pankod/refine-antd"
+import RefineReactRouter from "@pankod/refine-react-router-v6";
 
 type TitleProps = {
     collapsed: boolean;
 };
+const { Link } = RefineReactRouter;
 
 export const Title: React.FC<TitleProps> = ({collapsed}) => {
     return (
-        <Logo>
-            {collapsed ? (
-                <Icons.MenuOutlined style={{color: "white", fontSize: "32px"}}/>
-            ) : (
-                <img src="/images/logo.svg" alt="Finefood"/>
-            )}
-        </Logo>
+        <Link to={ "/" } >
+            <Logo>
+                {collapsed ? (
+                    <Icons.MenuOutlined style={{color: "white", fontSize: "32px"}}/>
+                ) : (
+                    <img src="/images/logo.svg" alt="Finefood"/>
+                )}
+            </Logo>
+        </Link>
     );
 };
