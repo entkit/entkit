b3f7d8ee3c578ba2c244498ed9a2f2b9eb1d29b0
Aaron Yordanyan <aaron.yor@gmail.com>
2023-04-10 12:47:03 +0400

    Server Generator, Prettier setup

---
+++ server-generator.go
@@ -0,0 +1,68 @@
+package entkit
+
+import (
+	"embed"
+	cp "github.com/otiai10/copy"
+	"io/fs"
+	"path"
+)
+
+var (
+	//go:embed server-templates/*
+	_serverTemplates embed.FS
+)
+
+type ServableAdapter interface {
+	BuildPath() string
+	RewritePath() string
+	StaticPaths() []string
+}
+
+type ServerAdapter struct{}
+
+var DefaultServerAdapter = ServerAdapter{}
+
+func (r ServerAdapter) GetName() string {
+	return "server"
+}
+
+func (r ServerAdapter) GetFS() fs.FS {
+	return _serverTemplates
+}
+
+func (r ServerAdapter) BeforeGen(generator *Generator) error {
+	for _, g := range generator.Extension.Generators {
+		servableGenerator, ok := g.Adapter.(ServableAdapter)
+		if !ok {
+			continue
+		}
+
+		if PBool(g.Serve) {
+			err := cp.Copy(
+				path.Join(PString(g.Path), servableGenerator.BuildPath()),
+				path.Join(PString(generator.Path), PString(g.Name)),
+			)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (r ServerAdapter) CommandAfterGen(generator *Generator) string {
+	return "go fmt"
+}
+
+func (r ServerAdapter) GetTemplates() []string {
+	return []string{
+		"server-templates/main.tmpl",
+		"server-templates/embed-fs.tmpl",
+	}
+}
+
+func (r ServerAdapter) GetStaticTemplates() []string {
+	return []string{
+		"server-templates/client.tmpl",
+	}
+}
